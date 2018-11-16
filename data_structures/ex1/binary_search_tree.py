f9a1e3047a8ccc3fbb7aec3eed2c8856b2c21a02
Andrew Liao <andrewoliao@gmail.com>
2018-11-16 13:40:16 -0800

    Implement depth_first

--- data_structures/ex1/binary_search_tree.py
+++ data_structures/ex1/binary_search_tree.py
@@ -7,13 +7,13 @@ class BinarySearchTree:
     self.right = None
      
   def depth_first_for_each(self, cb):
-    pass    
-    # print(self.value)
-    # if self.left:
-    #   self.left.depth_first_for_each(cb)
-    # if self.right:
-    #   self.right.depth_first_for_each(cb)
-    # return(cb(self))
+
+    cb(self.value)
+    if self.left:
+      self.left.depth_first_for_each(cb)
+    if self.right:
+      self.right.depth_first_for_each(cb)
+  
 
   def breadth_first_for_each(self, cb):
     q = Queue()
